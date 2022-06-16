e884cdf9fa382df32de596334896ff4271995963
giuseppegargani <sentierodisole1@yahoo.it>
2022-06-16 14:44:25 +0200

    inserito plugin e main

--- app/src/androidTest/java/com/example/experimental2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/experimental2/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.example.experimental2
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.instrumentedreport.GlobalTWClass
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,7 +15,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest: GlobalTWClass() {
     @Test
     fun useAppContext() {
         // Context of the app under test.
