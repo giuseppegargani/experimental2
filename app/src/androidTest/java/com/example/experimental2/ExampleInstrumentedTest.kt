c440efe6f849bdb68484f199d03d8a3040ad6dae
giuseppegargani <sentierodisole1@yahoo.it>
2022-06-16 14:16:58 +0200

    cambiato

--- app/src/androidTest/java/com/example/experimental2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/experimental2/ExampleInstrumentedTest.kt
@@ -2,7 +2,6 @@ package com.example.experimental2
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.instrumentedreport.GlobalTWClass
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,7 +14,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest: GlobalTWClass() {
+class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
